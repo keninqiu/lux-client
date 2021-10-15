42f1a764579397acf3f631051c2c3be5a14f3112
Ken Qiu <kenin.qiu@gmail.com>
2021-10-14 22:43:25 -0400

    f

--- src/app/survey/job/job.component.ts
+++ src/app/survey/job/job.component.ts
@@ -5,15 +5,15 @@ import { Router } from '@angular/router';
   selector: 'app-job',
   templateUrl: './job.component.html',
   styleUrls: [
-    './job.component.scss',
-    '../../../assets/css/market-worth/main.css'
+    './job.component.scss'
   ]
 })
 export class JobComponent implements OnInit {
-
+  editCountry: boolean;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.editCountry = false;
   }
 
   next() {
